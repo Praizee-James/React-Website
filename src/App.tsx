669ae57a5f4efd2d1163a3ab1e8ad50bbd62522a
Opeyemi James <opeyemijames857@gmail.com>
2025-10-20 16:18:24 +0100

    Newsletter and hero components addition

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 // import React from 'react'
 import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Analytics from "./components/Analytics"
+import Newsletter from "./components/Newsletter"
 function App() {
   
 
@@ -7,6 +10,9 @@ function App() {
     <>
       <div>
         <Navbar/>
+        <Hero/>
+        <Analytics/>
+        <Newsletter/>
       </div>
      
     </>
---
+++ src/components/Analytics.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Desktop  from '../assets/Desktop.jpg'
+const Analytics = () => {
+  return (
+    <div className='w-full bg-white py-16 px-4'>
+      <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
+<img className='w-[500px] mx-auto my-4' src={Desktop} alt="/" />
+<div className='flex flex-col justify-center p-4'>
+    <p className='text-[#00df9a] md:text-4xl sm:text-3xl text-2xl font-bold'>DATA ANALYTICS DASHBOARD</p>
+    <h1 className='md:text-3xl sm:text-3xl text-xl font-bold py-2'>Manage Data Analytics Centrally</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni molestiae voluptatum ratione cumque architecto corporis veniam magnam et possimus aspernatur, sit alias in facere id saepe, asperiores excepturi velit. Nemo.</p>
+ <button className="bg-black w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3 text-[#00df9a]">Get Started</button>
+</div>
+      </div>
+    </div>
+  )
+}
+
+export default Analytics
